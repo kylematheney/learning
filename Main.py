96bfc4fe2787a633b9eaa7a3d6522523ac24be15
Kyle Matheney <kylematheney@gmail.com>
2019-01-15 20:35:47 -0600

    First code commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+def get_credentials():
+    global username
+    global password
+
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
+
+
+get_credentials()  # run the function
+
+print(username, " ", password)  # Test
